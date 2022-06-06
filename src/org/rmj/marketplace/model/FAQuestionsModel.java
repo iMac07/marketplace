e08d8a0ab2f13718e89c250b7c778812b682a863
mnv121 <valenciamaynardn@gmail.com>
2022-06-06 10:14:26 +0800

    FAQuestion UI Adjustment / Integration
    added code for calling Class of FAQuestion
    minor adjustment for ui chat box

--- src/org/rmj/marketplace/model/FAQuestionsModel.java
+++ src/org/rmj/marketplace/model/FAQuestionsModel.java
@@ -11,116 +11,116 @@ import javafx.beans.property.SimpleStringProperty;
  * @author User
  */
 public class FAQuestionsModel {
-    private SimpleStringProperty repliedIndex01;   
-    private SimpleStringProperty repliedIndex02; 
-    private SimpleStringProperty repliedIndex03;  
-    private SimpleStringProperty repliedIndex04;    
-    private SimpleStringProperty repliedIndex05;   
-    private SimpleStringProperty repliedIndex06;  
-    private SimpleStringProperty repliedIndex07;  
-    private SimpleStringProperty repliedIndex08;    
-    private SimpleStringProperty repliedIndex09;   
-    private SimpleStringProperty repliedIndex10;  
-    private SimpleStringProperty repliedIndex11;  
-    private SimpleStringProperty repliedIndex12;  
-    private SimpleStringProperty repliedIndex13;   
-    private SimpleStringProperty repliedIndex14;  
-    private SimpleStringProperty repliedIndex15;  
-    private SimpleStringProperty repliedIndex16;    
-    private SimpleStringProperty repliedIndex17;    
+    private SimpleStringProperty RepliedIndex01;   
+    private SimpleStringProperty RepliedIndex02; 
+    private SimpleStringProperty RepliedIndex03;  
+    private SimpleStringProperty RepliedIndex04;    
+    private SimpleStringProperty RepliedIndex05;   
+    private SimpleStringProperty RepliedIndex06;  
+    private SimpleStringProperty RepliedIndex07;  
+    private SimpleStringProperty RepliedIndex08;    
+    private SimpleStringProperty RepliedIndex09;   
+    private SimpleStringProperty RepliedIndex10;  
+    private SimpleStringProperty RepliedIndex11;  
+    private SimpleStringProperty RepliedIndex12;  
+    private SimpleStringProperty RepliedIndex13;   
+    private SimpleStringProperty RepliedIndex14;  
+    private SimpleStringProperty RepliedIndex15;  
+    private SimpleStringProperty RepliedIndex16;    
+    private SimpleStringProperty RepliedIndex17;    
       
 
 
-    public FAQuestionsModel(String repliedIndex01, String repliedIndex02, String repliedIndex03, 
-            String repliedIndex04,String repliedIndex05,String repliedIndex06,
-            String repliedIndex07,String repliedIndex08,String repliedIndex09,
-            String repliedIndex10,String repliedIndex11,String repliedIndex12,
-            String repliedIndex13,String repliedIndex14,String repliedIndex15,
-            String repliedIndex16,String repliedIndex17){
-        this.repliedIndex01 = new SimpleStringProperty(repliedIndex01);
-        this.repliedIndex02 = new SimpleStringProperty(repliedIndex02);
-        this.repliedIndex03 = new SimpleStringProperty(repliedIndex03);
-        this.repliedIndex04 = new SimpleStringProperty(repliedIndex04);
-        this.repliedIndex05 = new SimpleStringProperty(repliedIndex05);
-        this.repliedIndex06 = new SimpleStringProperty(repliedIndex06);
-        this.repliedIndex07 = new SimpleStringProperty(repliedIndex07);
-        this.repliedIndex08 = new SimpleStringProperty(repliedIndex08);
-        this.repliedIndex09 = new SimpleStringProperty(repliedIndex09);
-        this.repliedIndex10 = new SimpleStringProperty(repliedIndex10);
-        this.repliedIndex11 = new SimpleStringProperty(repliedIndex11);
-        this.repliedIndex12 = new SimpleStringProperty(repliedIndex12);
-        this.repliedIndex13 = new SimpleStringProperty(repliedIndex13);
-        this.repliedIndex14 = new SimpleStringProperty(repliedIndex14);
-        this.repliedIndex15 = new SimpleStringProperty(repliedIndex15);
-        this.repliedIndex16 = new SimpleStringProperty(repliedIndex16);
-        this.repliedIndex17 = new SimpleStringProperty(repliedIndex17);
+    public FAQuestionsModel(String RepliedIndex01, String RepliedIndex02, String RepliedIndex03, 
+            String RepliedIndex04,String RepliedIndex05,String RepliedIndex06,
+            String RepliedIndex07,String RepliedIndex08,String RepliedIndex09,
+            String RepliedIndex10,String RepliedIndex11,String RepliedIndex12,
+            String RepliedIndex13,String RepliedIndex14,String RepliedIndex15,
+            String RepliedIndex16,String RepliedIndex17){
+        this.RepliedIndex01 = new SimpleStringProperty(RepliedIndex01);
+        this.RepliedIndex02 = new SimpleStringProperty(RepliedIndex02);
+        this.RepliedIndex03 = new SimpleStringProperty(RepliedIndex03);
+        this.RepliedIndex04 = new SimpleStringProperty(RepliedIndex04);
+        this.RepliedIndex05 = new SimpleStringProperty(RepliedIndex05);
+        this.RepliedIndex06 = new SimpleStringProperty(RepliedIndex06);
+        this.RepliedIndex07 = new SimpleStringProperty(RepliedIndex07);
+        this.RepliedIndex08 = new SimpleStringProperty(RepliedIndex08);
+        this.RepliedIndex09 = new SimpleStringProperty(RepliedIndex09);
+        this.RepliedIndex10 = new SimpleStringProperty(RepliedIndex10);
+        this.RepliedIndex11 = new SimpleStringProperty(RepliedIndex11);
+        this.RepliedIndex12 = new SimpleStringProperty(RepliedIndex12);
+        this.RepliedIndex13 = new SimpleStringProperty(RepliedIndex13);
+        this.RepliedIndex14 = new SimpleStringProperty(RepliedIndex14);
+        this.RepliedIndex15 = new SimpleStringProperty(RepliedIndex15);
+        this.RepliedIndex16 = new SimpleStringProperty(RepliedIndex16);
+        this.RepliedIndex17 = new SimpleStringProperty(RepliedIndex17);
     }
-    public String getrepliedIndex01() {
-        return repliedIndex01.get();
-    }
-
-    public String getrepliedIndex02() {
-        return repliedIndex02.get();
+    public String getRepliedIndex01() {
+        return RepliedIndex01.get();
     }
 
-    public String getrepliedIndex03() {
-        return repliedIndex03.get();
+    public String getRepliedIndex02() {
+        return RepliedIndex02.get();
     }
 
-    public String getrepliedIndex04() {
-        return repliedIndex04.get();
+    public String getRepliedIndex03() {
+        return RepliedIndex03.get();
     }
 
-    public String getrepliedIndex05() {
-        return repliedIndex05.get();
+    public String getRepliedIndex04() {
+        return RepliedIndex04.get();
     }
 
-    public String getrepliedIndex06() {
-        return repliedIndex06.get();
+    public String getRepliedIndex05() {
+        return RepliedIndex05.get();
     }
 
-    public String getrepliedIndex07() {
-        return repliedIndex07.get();
+    public String getRepliedIndex06() {
+        return RepliedIndex06.get();
     }
 
-    public String getrepliedIndex08() {
-        return repliedIndex08.get();
+    public String getRepliedIndex07() {
+        return RepliedIndex07.get();
     }
 
-    public String getrepliedIndex09() {
-        return repliedIndex09.get();
+    public String getRepliedIndex08() {
+        return RepliedIndex08.get();
     }
 
-    public String getrepliedIndex10() {
-        return repliedIndex10.get();
+    public String getRepliedIndex09() {
+        return RepliedIndex09.get();
     }
 
-    public String getrepliedIndex11() {
-        return repliedIndex11.get();
+    public String getRepliedIndex10() {
+        return RepliedIndex10.get();
     }
 
-    public String getrepliedIndex12() {
-        return repliedIndex12.get();
+    public String getRepliedIndex11() {
+        return RepliedIndex11.get();
     }
 
-    public String getrepliedIndex13() {
-        return repliedIndex13.get();
+    public String getRepliedIndex12() {
+        return RepliedIndex12.get();
     }
 
-    public String getrepliedIndex14() {
-        return repliedIndex14.get();
+    public String getRepliedIndex13() {
+        return RepliedIndex13.get();
     }
 
-    public String getrepliedIndex15() {
-        return repliedIndex15.get();
+    public String getRepliedIndex14() {
+        return RepliedIndex14.get();
     }
 
-    public String getrepliedIndex16() {
-        return repliedIndex16.get();
+    public String getRepliedIndex15() {
+        return RepliedIndex15.get();
     }
 
-    public String getrepliedIndex17() {
-        return repliedIndex17.get();
+    public String getRepliedIndex16() {
+        return RepliedIndex16.get();
+    }
+
+    public String getRepliedIndex17() {
+        return RepliedIndex17.get();
     }
 
 }
