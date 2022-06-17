8d553b7e46e275e280a9c436ff9c0c56bc53de12
Jeitee <93184955+jeitee@users.noreply.github.com>
2022-06-17 14:10:56 +0800

    RefactorOrderDetail
    * Display order history on client info
    * display order Details

--- src/org/rmj/marketplace/controller/ClientInfoController.java
+++ src/org/rmj/marketplace/controller/ClientInfoController.java
@@ -6,11 +6,12 @@
 package org.rmj.marketplace.controller;
 
 import com.sun.javafx.scene.control.skin.TableHeaderRow;
+import java.io.IOException;
 import org.rmj.marketplace.model.ScreenInterface;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.DateTimeException;
 import java.util.Date;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,9 +21,13 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Pagination;
 import javafx.scene.control.TableColumn;
@@ -32,7 +37,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import static junit.framework.Assert.fail;
 import org.rmj.marketplace.model.OrderModel;
 import org.rmj.appdriver.GRider;
@@ -53,13 +62,20 @@ public class ClientInfoController implements Initializable, ScreenInterface {
     private int orderRow = 0;
     private int clienrRow = 0;
     private int dataSize;
+    private final String pxeModuleName = "Client Information";
+    private double xOffset = 0;
+    private double yOffset = 0;
     
     private LTransaction  oListener;
     private Clients oTrans;
+    private int pagecounter;
     
     private boolean pbLoaded = false;
     private int pnRow = -1;
+    private int pnRow1 = -1;
     private String category;
+    private int oldPnRow = -1;
+    private String oldTransNox = "";
     
     @FXML
     private Pagination tblPagination;
@@ -116,7 +132,7 @@ public class ClientInfoController implements Initializable, ScreenInterface {
     @FXML
     private Pagination pagination;
     
-    private static final int ROWS_PER_PAGE = 20;
+    private static final int ROWS_PER_PAGE = 10;
     
 //    private final TableView<ClientInfoModel> table = createTable();
 //    private final List<ClientInfoModel> data = createData();     
@@ -142,6 +158,7 @@ public class ClientInfoController implements Initializable, ScreenInterface {
 //        txtSeeks11.setOnKeyPressed(this::txtField_KeyPressed);
         
         loadClient();
+        loadOrders(oldTransNox);
         pagination.setPageFactory(this::createPage); 
         btnRefresh.setOnAction(this::cmdButton_Click);    
             
@@ -190,8 +207,9 @@ public class ClientInfoController implements Initializable, ScreenInterface {
                             (String) oTrans.getDetail(lnCtr, 19),
                             (String) oTrans.getDetail(lnCtr, 16),
                             (String) oTrans.getDetail(lnCtr, 17),
-                            (String) oTrans.getDetail(lnCtr, "sHouseNox") +  ", " +oTrans.getDetail(lnCtr, "sAddressx") +  ", " + oTrans.getDetail(lnCtr, "xTownName"),
+                            (String) oTrans.getDetail(lnCtr, "sHouseNox") +  ", " + oTrans.getDetail(lnCtr, "sAddressx") +  ", " + oTrans.getDetail(lnCtr, "xTownName"),
                             (String) oTrans.getDetail(lnCtr, "cGenderCd"),
+                            (String) oTrans.getDetail(lnCtr, "xNational"),
                             (String) oTrans.getDetail(lnCtr, 2),
                             (CommonUtils.xsDateLong((Date) oTrans.getDetail(lnCtr, "dBirthDte"))),
                             (String) oTrans.getDetail(lnCtr, "xBirthPlc"),
@@ -211,12 +229,53 @@ public class ClientInfoController implements Initializable, ScreenInterface {
         oApp = foValue; //To change body of generated methods, choose Tools | Templates.
     }
     
+    private void loadOrders(String transNox){
+        int lnCtr;
+        try {
+            oldTransNox = transNox;
+            System.out.println(oldTransNox + " old");
+            order_data.clear();
+            if (oTrans.LoadOrder(transNox)){
+                //true if by barcode; false if by description
+                oTrans.displayMasFields();
+                for (lnCtr = 1; lnCtr <= oTrans.getOrderItemCount(); lnCtr++){
+                    order_data.add(new OrderModel(String.valueOf(lnCtr),
+                            (String) oTrans.getOrder(lnCtr, "sTransNox"),
+                            (CommonUtils.xsDateLong((Date) oTrans.getOrder(lnCtr, "dTransact"))),
+                            (String) oTrans.getOrder(lnCtr, "sTermCode"),
+                            oTrans.getOrder(lnCtr, "nTranTotl").toString(),
+                            oTrans.getOrder(lnCtr, "nVATRatex").toString(),
+                            oTrans.getOrder(lnCtr, "nDiscount").toString(),
+                            oTrans.getOrder(lnCtr, "nAddDiscx").toString(),
+                            oTrans.getOrder(lnCtr, "nFreightx").toString(),
+                            oTrans.getOrder(lnCtr, "nAmtPaidx").toString(),
+                            oTrans.getOrder(lnCtr, "cTranStat").toString(),
+                            (String) oTrans.getOrder(lnCtr, "sRemarksx"),
+                            (String) oTrans.getOrder(lnCtr, "sCompnyNm"),
+                            (String) oTrans.getOrder(lnCtr, "sAddressx"),
+                            (String) oTrans.getOrder(lnCtr, "sTownName")));
+                            System.out.println(oTrans.getOrder(lnCtr, "sTransNox"));
+                            System.out.println(oTrans.getOrder(lnCtr, "nTranTotl"));
+                }   
+                initOrderGrid();
+            } else {//true if by barcode; false if by description
+            }    
+        } catch (SQLException ex) {
+            System.out.println("SQLException" + ex.getMessage());
+        } catch (NullPointerException ex) {
+            System.out.println("NullPointerException" + ex.getMessage());
+        } catch (DateTimeException ex) {
+//            MsgBox.showOk(ex.getMessage());
+            System.out.println("DateTimeException" + ex.getMessage());
+        } 
+    }
+    
     public void initClientsGrid() { 
         clientIndex01.setStyle("-fx-alignment: CENTER-LEFT; -fx-padding: 0 0 0 5;");
         clientIndex02.setStyle("-fx-alignment: CENTER-LEFT; -fx-padding: 0 0 0 5;");
         clientIndex03.setStyle("-fx-alignment: CENTER-LEFT; -fx-padding: 0 0 0 5;");
         
-        clientIndex01.setCellValueFactory(new PropertyValueFactory<>("clientIndex10"));
+        clientIndex01.setCellValueFactory(new PropertyValueFactory<>("clientIndex11"));
         clientIndex02.setCellValueFactory(new PropertyValueFactory<>("clientIndex02"));
         clientIndex03.setCellValueFactory(new PropertyValueFactory<>("clientIndex03"));
         tblClients.widthProperty().addListener((ObservableValue<? extends Number> source, Number oldWidth, Number newWidth) -> {
@@ -239,6 +298,41 @@ public class ClientInfoController implements Initializable, ScreenInterface {
         tblClients.setItems(sortedData);
         
     }
+     public void initOrderGrid() { 
+        orderIndex01.setStyle("-fx-alignment: CENTER-LEFT; -fx-padding: 0 0 0 5;");
+        orderIndex02.setStyle("-fx-alignment: CENTER-LEFT; -fx-padding: 0 0 0 5;");
+        orderIndex03.setStyle("-fx-alignment: CENTER-LEFT; -fx-padding: 0 0 0 5;");
+        orderIndex04.setStyle("-fx-alignment: CENTER-LEFT; -fx-padding: 0 0 0 5;");
+        orderIndex05.setStyle("-fx-alignment: CENTER-LEFT; -fx-padding: 0 0 0 5;");
+        
+        orderIndex01.setCellValueFactory(new PropertyValueFactory<>("orderIndex01"));
+        orderIndex02.setCellValueFactory(new PropertyValueFactory<>("orderIndex02"));
+        orderIndex03.setCellValueFactory(new PropertyValueFactory<>("orderIndex05"));
+        orderIndex04.setCellValueFactory(new PropertyValueFactory<>("orderIndex03"));
+        orderIndex05.setCellValueFactory(new PropertyValueFactory<>("orderIndex04"));
+        tblOrders.widthProperty().addListener((ObservableValue<? extends Number> source, Number oldWidth, Number newWidth) -> {
+            TableHeaderRow header = (TableHeaderRow) tblOrders.lookup("TableHeaderRow");
+            header.reorderingProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+                header.setReordering(false);
+            });
+        });
+        tblOrders.setItems(order_data);
+        
+//        filteredData = new FilteredList<>(order_data, b -> true);
+//        autoSearch(txtSeeks10);
+//        autoSearch(txtSeeks11);
+//        // 3. Wrap the FilteredList in a SortedList. 
+//        SortedList<ClientInfoModel> sortedData = new SortedList<>(filteredData);
+//
+//        // 4. Bind the SortedList comparator to the TableView comparator.
+//        // 	  Otherwise, sorting the TableView would have no effect.
+//        sortedData.comparatorProperty().bind(tblClients.comparatorProperty());
+//
+//        // 5. Add sorted (and filtered) data to the table.
+//        tblClients.setItems(sortedData);
+        
+                
+}
     
     private void autoSearch(TextField txtField){
         int lnIndex = Integer.parseInt(txtField.getId().substring(8, 10));
@@ -284,16 +378,21 @@ public class ClientInfoController implements Initializable, ScreenInterface {
     
     @FXML
     private void tblClients_Clicked() {
-        pnRow = tblClients.getSelectionModel().getSelectedIndex();
+        pnRow = tblClients.getSelectionModel().getSelectedIndex(); 
+        pagecounter = pnRow + pagination.getCurrentPageIndex() * ROWS_PER_PAGE;
         getSelectedItem();
-       
+        loadOrders(filteredData.get(pagecounter).getClientIndex08());
+
+        
         tblClients.setOnKeyReleased((KeyEvent t)-> {
                 KeyCode key = t.getCode();
                 switch (key){
                     case DOWN:
                         pnRow = tblClients.getSelectionModel().getSelectedIndex();
-                        if (pnRow == tblClients.getItems().size()) {
-                            pnRow = tblClients.getItems().size();
+                        pagecounter = pnRow + pagination.getCurrentPageIndex() * ROWS_PER_PAGE;
+                        loadOrders(filteredData.get(pagecounter).getClientIndex07());
+                        if (pagecounter == tblClients.getItems().size()) {
+                            pagecounter = tblClients.getItems().size();
                             getSelectedItem();
                         }else {
 //                            int y = 1;
@@ -305,6 +404,8 @@ public class ClientInfoController implements Initializable, ScreenInterface {
                         int pnRows = 0;
                         
                         pnRows = tblClients.getSelectionModel().getSelectedIndex();
+                        pagecounter = pnRow + pagination.getCurrentPageIndex() * ROWS_PER_PAGE;
+                        loadOrders(filteredData.get(pagecounter).getClientIndex07());
                             pnRow = pnRows; 
                             getSelectedItem();
                         break;
@@ -315,12 +416,12 @@ public class ClientInfoController implements Initializable, ScreenInterface {
     }
     
     private void getSelectedItem(){
-        int genval = Integer.parseInt((filteredData.get(pnRow).getClientIndex06()));
-        txtField01.setText(filteredData.get(pnRow).getClientIndex07());
-        txtField02.setText(filteredData.get(pnRow).getClientIndex02());
-        txtField03.setText(filteredData.get(pnRow).getClientIndex05());
-        txtField04.setText(filteredData.get(pnRow).getClientIndex04()); 
-        txtField05.setText(filteredData.get(pnRow).getClientIndex03()); 
+        int genval = Integer.parseInt((filteredData.get(pagecounter).getClientIndex06()));
+        txtField01.setText(filteredData.get(pagecounter).getClientIndex08());
+        txtField02.setText(filteredData.get(pagecounter).getClientIndex02());
+        txtField03.setText(filteredData.get(pagecounter).getClientIndex05());
+        txtField04.setText(filteredData.get(pagecounter).getClientIndex04()); 
+        txtField05.setText(filteredData.get(pagecounter).getClientIndex03()); 
         switch (genval) {
             case 1:
                 txtField06.setText("Female");
@@ -332,8 +433,9 @@ public class ClientInfoController implements Initializable, ScreenInterface {
                 txtField06.setText("LGBTQ");
                 break;
         }
-        txtField08.setText(filteredData.get(pnRow).getClientIndex08()); 
-        txtField09.setText(filteredData.get(pnRow).getClientIndex09());      
+        txtField07.setText(filteredData.get(pagecounter).getClientIndex07()); 
+        txtField08.setText(filteredData.get(pagecounter).getClientIndex09()); 
+        txtField09.setText(filteredData.get(pagecounter).getClientIndex10());      
     } 
     
     private void cmdButton_Click(ActionEvent event) {
@@ -348,4 +450,64 @@ public class ClientInfoController implements Initializable, ScreenInterface {
                     break;
                 }
     } 
+    @FXML
+    private void tblOrderDetail_Clicked() {
+        try { 
+            pnRow1 = tblOrders.getSelectionModel().getSelectedIndex();
+            System.out.print(pnRow1);
+            loadOrderDetail(order_data.get(pnRow1).getOrderIndex02(), pnRow1 + 1); 
+        } catch (SQLException ex) {
+            Logger.getLogger(ClientInfoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+    private void loadOrderDetail(String fsCode, int fnRow) throws SQLException{
+        try {
+            Stage stage = new Stage();
+            
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/org/rmj/marketplace/view/OrderDetail.fxml"));
+
+            OrderDetailController loControl = new OrderDetailController();
+            loControl.setGRider(oApp);
+            loControl.setOrderDetailObject(oTrans);
+            loControl.setOrderDetailCode(fsCode);
+            loControl.setTableRow(fnRow);
+            loControl.setData(order_data.get(pnRow1));
+            loControl.setCltData(filteredData.get(pnRow));
+            
+            fxmlLoader.setController(loControl);
+            
+            //load the main interface
+            Parent parent = fxmlLoader.load();
+                
+            parent.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+            parent.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    stage.setX(event.getScreenX() - xOffset);
+                    stage.setY(event.getScreenY() - yOffset);
+                }
+            });
+            
+            //set the main interface as the scene
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("");
+            stage.showAndWait();
+            
+            loadOrders(filteredData.get(pnRow).getClientIndex07());
+        } catch (IOException e) {
+            e.printStackTrace();
+            MsgBox.showOk(e.getMessage());
+            System.exit(1);
+        }
+    }
 }
--- src/org/rmj/marketplace/controller/OrderDetailController.java
+++ src/org/rmj/marketplace/controller/OrderDetailController.java
@@ -5,11 +5,35 @@
  */
 package org.rmj.marketplace.controller;
 
+import com.sun.javafx.scene.control.skin.TableHeaderRow;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import org.rmj.marketplace.model.ScreenInterface;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import org.rmj.appdriver.GRider;
+import org.rmj.appdriver.agentfx.CommonUtils;
+import org.rmj.appdriver.agentfx.ShowMessageFX;
+import org.rmj.marketplace.base.Clients;
+import static org.rmj.marketplace.controller.OrderProcessingController.priceWithDecimal;
+import org.rmj.marketplace.model.ClientInfoModel;
+import org.rmj.marketplace.model.OrderDetailModel;
+import org.rmj.marketplace.model.OrderModel;
 
 /**
  * FXML Controller class
@@ -19,17 +43,205 @@ import org.rmj.appdriver.GRider;
 public class OrderDetailController implements Initializable, ScreenInterface {
     
     private GRider oApp;
-
+    private Clients oTrans;
+    
+    public int tbl_row = 0;
+    private String psCode;
+    private String clientID;
+    private boolean state = false;
+    public static OrderModel orderModel;
+    private ObservableList<OrderDetailModel> orderdetail_data = FXCollections.observableArrayList();
+    public static ClientInfoModel  cltModel;
+    private final String pxeModuleName = "Order Detail";
+    double total = 0;
+    
+    @FXML
+    private Button btnExit;
+    @FXML
+    private FontAwesomeIconView glyphExit;
+    @FXML
+    private AnchorPane searchBar;
+    @FXML
+    private Label lblOrderDate;
+    @FXML
+    private Label lblVatperc;
+    @FXML
+    private Label lblVat;
+    @FXML
+    private Label lblVatLess;
+    @FXML
+    private Label lblSubtotl;
+    @FXML
+    private Label lblSubtotlAmt;
+    @FXML
+    private Label lblShipFee;
+    @FXML
+    private Label lblReference;
+    @FXML
+    private Label lblCustAdd;
+    @FXML
+    private Label lblCustName;
+    @FXML
+    private Label lblSellerName;
+    @FXML
+    private Label lblSellerAdd;
+    @FXML
+    private Label lblContactNo;
+    @FXML
+    private TableView<OrderDetailModel> tblOrders;
+    @FXML
+    private TableColumn<?, ?> orderIndex01;
+    @FXML
+    private TableColumn<?, ?> orderIndex02;
+    @FXML
+    private TableColumn<?, ?> orderIndex03;
+    @FXML
+    private TableColumn<?, ?> orderIndex04;
+    @FXML
+    private TableColumn<?, ?> orderIndex05;
+    @FXML
+    private TableColumn<?, ?> orderIndex06;
+    @FXML
+    private TableColumn<?, ?> orderIndex07;
+    @FXML
+    private TableColumn<?, ?> orderIndex08;
+    @FXML
+    private Label lblTotal;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
+        btnExit.setOnAction(this::cmdButton_Click);
+        try {
+            
+            loadIOrderDetails();
+            System.out.println(psCode);
+            loadOrderdDetails(psCode);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDetailController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
 
     @Override
     public void setGRider(GRider foValue) {
-        oApp = foValue; //To change body of generated methods, choose Tools | Torderlates.
+        oApp = foValue; //To change bod y of generated methods, choose Tools | Torderlates.
+    }
+     public void setOrderDetailObject(Clients foValue){
+        oTrans = foValue;
+    }
+    public void setOrderDetailCode(String fsValue){
+        psCode = fsValue;
+    }
+    public void setTableRow(int row){
+        tbl_row = row;
+    }
+    
+    public void setState(boolean fsValue){
+        state = fsValue;
+    }
+    public static void setData(OrderModel orderdata){
+        orderModel = orderdata;    
+    }
+    public static void setCltData(ClientInfoModel cltID){
+        cltModel = cltID;    
+    }
+    private void cmdButton_Click(ActionEvent event) {
+        String lsButton = ((Button)event.getSource()).getId();
+        switch (lsButton){
+            case "btnExit":
+                CommonUtils.closeStage(btnExit);
+                break;
+                
+            default:
+                ShowMessageFX.Warning(null, pxeModuleName, "Button with name " + lsButton + " not registered.");
+                return;
+        }
+    } 
+    private void loadOrderdDetails(String strans) throws SQLException{
+        
+            orderdetail_data.clear();
+           
+            if (oTrans.LoadOrderDetail(strans)){
+                 
+                 
+                int lnRow = oTrans.getOrderDetailItemCount(); 
+                for (int lnCtr = 1; lnCtr <= lnRow; lnCtr++){
+                    //get
+                    total += (Double.parseDouble(oTrans.getDetailOrder(lnCtr, "nUnitPrce").toString()) * Double.parseDouble(oTrans.getDetailOrder(lnCtr, "nQuantity").toString()));
+                    orderdetail_data.add(new OrderDetailModel(String.valueOf(lnCtr),
+                    (String) oTrans.getDetailOrder(lnCtr, "sTransNox"),
+                            (String) oTrans.getDetailOrder(lnCtr, "xDescript"),
+                            (String) oTrans.getDetailOrder(lnCtr, "xBrandNme") +  ", " + oTrans.getDetailOrder(lnCtr, "xModelNme"),
+                            (String) oTrans.getDetailOrder(lnCtr, "xColorNme"),
+                            oTrans.getDetailOrder(lnCtr, "nQuantity").toString(),
+                            priceWithDecimal(Double.valueOf(oTrans.getDetailOrder(lnCtr, "nUnitPrce").toString())),
+                            priceWithDecimal(total),
+                            "",
+                            "",
+                            "",
+                            ""));
+                            System.out.print((CommonUtils.NumberFormat((Number)total, "#,##0.00")));
+                    
+                }
+                initGrid();
+            }
+    }
+    
+    public void loadIOrderDetails(){
+        Double vat = 0.0;
+        Double vatAmt = 0.0;
+        Double vatLess = 0.0;
+        Double subTotl = 0.0;
+        Double Totl = 0.0;
+        vat += (Double.parseDouble(orderModel.getOrderIndex06())* 100);
+        vatAmt += (Double.parseDouble(orderModel.getOrderIndex06()) * Double.parseDouble(orderModel.getOrderIndex05()));
+        vatLess += (Double.parseDouble(orderModel.getOrderIndex05())- vatAmt);
+        subTotl = (Double.parseDouble(orderModel.getOrderIndex05()));
+        Totl +=  (vatLess + vatAmt);
+        lblReference.setText(orderModel.getOrderIndex02());
+        lblOrderDate.setText(orderModel.getOrderIndex03());
+        lblVatperc.setText("VAT " +(String.valueOf(vat) + " %"));
+        lblVat.setText(priceWithDecimal(vatAmt));
+        lblVatLess.setText(priceWithDecimal(vatLess));
+        lblSubtotlAmt.setText(priceWithDecimal(subTotl));
+        lblTotal.setText(priceWithDecimal(Totl));
+        
+       lblCustName.setText(cltModel.getClientIndex02());
+       lblCustAdd.setText(cltModel.getClientIndex05());
+       lblContactNo.setText(cltModel.getClientIndex03());
+       psCode = (orderModel.getOrderIndex02());
+    }
+    
+    private void initGrid() {
+        
+        orderIndex01.setStyle("-fx-alignment: CENTER;" );
+        orderIndex02.setStyle("-fx-alignment: CENTER-LEFT;-fx-padding: 0 0 0 5;");
+        orderIndex03.setStyle("-fx-alignment: CENTER-LEFT;-fx-padding: 0 0 0 5;");
+        orderIndex04.setStyle("-fx-alignment: CENTER-LEFT;-fx-padding: 0 0 0 5;");
+        orderIndex05.setStyle("-fx-alignment: CENTER-LEFT;-fx-padding: 0 0 0 5;");
+        orderIndex06.setStyle("-fx-alignment: CENTER-LEFT;-fx-padding: 0 0 0 5;");
+        orderIndex07.setStyle("-fx-alignment: CENTER-LEFT;-fx-padding: 0 0 0 5;");
+        orderIndex08.setStyle("-fx-alignment: CENTER-LEFT;-fx-padding: 0 0 0 5;");
+        
+        orderIndex01.setCellValueFactory(new PropertyValueFactory<>("orderIndex01"));
+        orderIndex02.setCellValueFactory(new PropertyValueFactory<>("orderIndex02"));
+        orderIndex03.setCellValueFactory(new PropertyValueFactory<>("orderIndex03"));
+        orderIndex04.setCellValueFactory(new PropertyValueFactory<>("orderIndex04"));
+        orderIndex05.setCellValueFactory(new PropertyValueFactory<>("orderIndex05"));
+        orderIndex06.setCellValueFactory(new PropertyValueFactory<>("orderIndex06"));
+        orderIndex07.setCellValueFactory(new PropertyValueFactory<>("orderIndex07"));
+        orderIndex08.setCellValueFactory(new PropertyValueFactory<>("orderIndex08"));
+      
+        tblOrders.widthProperty().addListener((ObservableValue<? extends Number> source, Number oldWidth, Number newWidth) -> {
+            TableHeaderRow header = (TableHeaderRow) tblOrders.lookup("TableHeaderRow");
+            header.reorderingProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+                header.setReordering(false);
+            });
+        });
+        tblOrders.setItems(orderdetail_data);
     }
 }
+
